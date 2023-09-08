17b04e9f7db4e33ca9118d5740375e83ed39e82c
Isrealshecktar <Iamshecktar1996@gmail.com>
2023-09-08 10:00:34 +0100

    header files for the entire shell

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void shecktar_write(const char *input);
+void my_prompt(void);
+void Ex_prompt(const char *command);
+char *take_inputs(char *input, size_t insize, FILE *stream);
+void tkn_prompt(char *input);
+char *find_cmd_path(const char *command);
+
+
+/**********externs***********/
+extern char **environ;
+
+#endif /* SHELL_H */

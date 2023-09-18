02586499fdf8e2d036d1ad285ec95614672eb512
mosignorjude <mosignorjude2@gmail.com>
2023-09-18 06:46:58 -0700

    added a function

---
+++ memcpy.c
@@ -0,0 +1,17 @@
+/**
+ * _memcpy - fill memory with a constant byte
+ * @dest: pointer to the address of the memory to store the copied data 
+ * @src: pointer to the  address of the memory from which the data will be copied.
+ * @n: number of characters to copy
+ * Return: a pointer to the memory area.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

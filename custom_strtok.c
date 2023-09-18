696cafab4f4118849053576c59771d409b10d632
mosignorjude <mosignorjude2@gmail.com>
2023-09-18 16:14:38 -0700

    changes to strtok function

--- custom_strtok.c
+++ custom_strtok.c
@@ -1,13 +1,13 @@
 char *customStrtok(char *str, const char *delim) {
-    static char *token; /* Static variable to store the position between calls*/
+    static char *token;
     if (str != NULL) {
         token = str;
     } else if (token == NULL) {
-        return NULL; /* If no string provided and no previous position, return NULL*/
+        return NULL;
     }
 
     char *start = token;
-    token = strpbrk(start, delim); /*Find the next delimiter in the remaining string*/
+    token = strpbrk(start, delim);
 
     if (token != NULL) {
         *token = '\0'; /*Replace the delimiter with null terminator*/

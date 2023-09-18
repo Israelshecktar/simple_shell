28196bd1230d0a5e004863e87dd374f90d0b626a
mosignorjude <mosignorjude2@gmail.com>
2023-09-18 16:01:34 -0700

    getline function

---
+++ getlineFunction.c
@@ -0,0 +1,47 @@
+char *customGetline() {
+    char *line = NULL;
+    char c;
+    size_t len = 0;
+    size_t capacity = 0;
+
+    while (1) {
+        c = getchar();
+
+        if (c == '\n' || c == EOF) {
+            if (len > 0) {
+                break; // Stop reading when a newline or EOF is encountered
+            } else if (c == EOF) {
+                free(line);
+                return NULL; // Return NULL if no input was read before EOF
+            }
+        }
+
+        if (len + 1 >= capacity) {
+            capacity += 10; // Increase capacity in chunks
+            char *newLine = realloc(line, capacity);
+            if (newLine == NULL) {
+                perror("Error (realloc)");
+                free(line);
+                return NULL;
+            }
+            line = newLine;
+        }
+
+        line[len++] = c;
+    }
+
+    // Null-terminate the string
+    if (len + 1 >= capacity) {
+        capacity += 1;
+        char *newLine = realloc(line, capacity);
+        if (newLine == NULL) {
+            perror("Error (realloc)");
+            free(line);
+            return NULL;
+        }
+        line = newLine;
+    }
+    line[len] = '\0';
+
+    return line;
+}

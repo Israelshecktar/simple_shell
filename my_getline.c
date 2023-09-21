43e5eea604983730034c0ba604e6388019ea94e5
Israelshecktar <Iamshecktar1996@gmaiil.com>
2023-09-22 00:57:00 +0100

    fixed getline

--- my_getline.c
+++ my_getline.c
@@ -2,28 +2,39 @@
 #define READ_SIZE 1024
 
 /**
-* shecktar_getline - function reads the entire line from the file stream
-* @buffer: buffer in which to store read chars
-* @size: buffer size
-* @stream: file stream from which to read
-* Return: buffer on success, Null on failure or EOF
-*/
+ * shecktar_getline - function reads the entire line from the file stream
+ * @buffer: buffer in which to store read chars
+ * @size: buffer size
+ * @stream: file stream from which to read
+ * Return: buffer on success, Null on failure or EOF
+ */
 char *shecktar_getline(char *buffer, size_t size, FILE *stream)
 {
+	static char internalBuffer[READ_SIZE];
+	static char *ptr;
+	static int charsLeft;
 	size_t i = 0;
-	int ch;
 
 	if (!buffer || size <= 0 || !stream)
 		return (NULL);
 
-	while ((ch = fgetc(stream)) != EOF && i < size - 1)
+	while (i < size - 1)
 	{
-		buffer[i++] = ch;
-		if (ch == '\n')
-			break;
-	}
+		if (charsLeft <= 0)
+		{
+			charsLeft = read(fileno(stream), internalBuffer, READ_SIZE);
+			if (charsLeft <= 0)
+				break;
+			ptr = internalBuffer;
+		}
+		buffer[i++] = *ptr++;
+		charsLeft--;
 
-	if (i == 0 && ch == EOF)
+		if (buffer[i - 1] == '\n')
+			break;
+
+	}
+	if (i == 0)
 		return (NULL);
 
 	buffer[i] = '\0';
